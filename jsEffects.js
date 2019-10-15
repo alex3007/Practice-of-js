f09ad999a35bb7582f3c3085f4518f3e95d6f4e3
alex3007 <48695087+alex3007@users.noreply.github.com>
2019-10-15 15:14:15 +0300

    modified 15-10-19

--- jsEffects.js
+++ jsEffects.js
@@ -43,7 +43,62 @@ $("a").css("color", "darkblue");
 
 
 
-/*
+/*  // parallax effect
+    $.fn.bgscroll = function (options) {
+
+        var x = $.extend({
+            bgpositionx: 50,
+            direction: "bottom",
+            debug: !1,
+            min: 0,
+            max: 100
+        }, options);
+
+        var a = $(document).height() - $(window).height(),
+            b = a - (this.offset().top + this.height());
+
+        this.offset().top < a && (b = 0);
+
+        var c = (this.offset().top + this.height());
+
+        if ($(window).scrollTop() > b && $(window).scrollTop() < c) {
+            var d = ($(window).scrollTop() - b) / (c - b) * 100;
+
+            "top" == x.direction && (d = 100 - d),
+                d > x.max && (d = x.max),
+                d < x.min && (d = x.min);
+
+            if (x.debug) {
+                console.log('Element background position: ' + d + ' %');
+            }
+        }
+
+        return this.css({
+            backgroundPosition: x.bgpositionx + '% ' + d + '%'
+        });
+    };
+
+
+    // parallax settings  
+    $(window).scroll(function () {
+        $('.bg-1').bgscroll({
+            direction: 'bottom', // bottom or top
+            bgpositionx: 50, // x positio, 0..100, in %, 50 is center
+            debug: false,  
+            min: 0, // min position(%) of moving
+            max: 100 // max position(%) of moving
+        });
+        $('.bg-2').bgscroll({
+            direction: 'top'
+        });
+        $('.bg-3').bgscroll({
+            direction: 'bottom'
+        });
+    })*/
+    
+
+
+****************************************************
 
 var locations = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg"];
 var currentImage = 3;
